b26dc0e2a408883ca280bf3652c835b3be156e06
Hadrian Hughes <hadrianhughes@gmail.com>
2025-11-23 20:13:47 +0000

    Add a scrollbar to the commits pane

--- src/ui/commits_pane.rs
+++ src/ui/commits_pane.rs
@@ -1,13 +1,13 @@
 use ratatui::{
     buffer::Buffer,
-    layout::Rect,
+    layout::{Constraint, Direction, Layout, Rect},
     style::{
         palette::tailwind::SLATE,
         Modifier, Style, Stylize,
     },
     symbols::border,
     text::{Line, Text},
-    widgets::{Block, HighlightSpacing, List, ListItem, ListState, Padding, StatefulWidget, Widget},
+    widgets::{Block, HighlightSpacing, List, ListItem, ListState, Scrollbar, ScrollbarOrientation, ScrollbarState, StatefulWidget, Widget},
 };
 use textwrap::wrap;
 
@@ -17,20 +17,23 @@ use crate::core::Commit;
 pub struct CommitsPane<'a> {
     commits: &'a Vec<Commit>,
     has_focus: bool,
-    pub state: ListState,
+    pub list_state: ListState,
+    pub scroll_state: ScrollbarState,
 }
 
 impl<'a> CommitsPane<'a> {
     pub fn new(commits: &'a Vec<Commit>, has_focus: bool, selected_commit: usize) -> Self {
-        let mut pane = CommitsPane {
+        CommitsPane {
             commits,
             has_focus,
-            state: ListState::default(),
-        };
-
-        pane.state.select(Some(selected_commit));
-
-        pane
+            list_state: {
+                let mut l = ListState::default();
+                l.select(Some(selected_commit));
+                l
+            },
+            scroll_state: ScrollbarState::new(commits.len())
+                .position(selected_commit),
+        }
     }
 }
 
@@ -40,8 +43,17 @@ impl<'a> Widget for &mut CommitsPane<'a> {
 
         let block = Block::bordered()
             .title(title.centered())
-            .border_set(if self.has_focus { border::THICK } else { border::PLAIN })
-            .padding(Padding::uniform(1));
+            .border_set(if self.has_focus { border::THICK } else { border::PLAIN });
+
+        let inner = block.inner(area);
+
+        let layout_parts = Layout::default()
+            .direction(Direction::Horizontal)
+            .constraints([
+                Constraint::Fill(1),
+                Constraint::Length(1),
+            ])
+            .split(inner);
 
         let total_width = area.width as usize;
         let horizontal_padding = 2;
@@ -53,17 +65,17 @@ impl<'a> Widget for &mut CommitsPane<'a> {
             .map(|(_, item)| {
                 let mut parts = vec![
                     Line::from(""),
-                    Line::from(format!(" {}", item.hash.clone())),
-                    Line::from(format!(" {}", item.author.clone())),
+                    Line::from(format!(" {} ", item.hash.clone())),
+                    Line::from(format!(" {} ", item.author.clone())),
                 ];
 
                 if let Some(msg) = &item.message {
                     for line in msg.lines() {
                         if wrap_width == 0 {
-                            parts.push(Line::from(format!(" {}", line)));
+                            parts.push(Line::from(format!(" {} ", line)));
                         } else {
                             for wrapped in wrap(line, wrap_width) {
-                                parts.push(Line::from(format!(" {}", wrapped)));
+                                parts.push(Line::from(format!(" {} ", wrapped)));
                             }
                         }
                     }
@@ -75,11 +87,17 @@ impl<'a> Widget for &mut CommitsPane<'a> {
             })
             .collect();
 
+        block.render(area, buf);
+
         let list = List::new(items)
-            .block(block)
             .highlight_style(Style::new().bg(if self.has_focus { SLATE.c600 } else { SLATE.c700 }).add_modifier(Modifier::BOLD))
             .highlight_spacing(HighlightSpacing::Always);
 
-        StatefulWidget::render(list, area, buf, &mut self.state)
+        StatefulWidget::render(list, layout_parts[0], buf, &mut self.list_state);
+
+        Scrollbar::new(ScrollbarOrientation::VerticalRight)
+            .begin_symbol(Some("↑"))
+            .end_symbol(Some("↓"))
+            .render(layout_parts[1], buf, &mut self.scroll_state)
     }
 }
