0bafa6f01795eb053b2acf86dbe35e5b51d1e668
Hadrian Hughes <hadrianhughes@gmail.com>
2025-11-28 18:00:29 +0000

    Improve and simplify diff_pane render algorithm

--- src/ui/diff_pane.rs
+++ src/ui/diff_pane.rs
@@ -1,6 +1,6 @@
 use std::collections::HashMap;
 use ratatui::{
-    buffer::Buffer, layout::Rect, style::{Color, Style, Stylize}, symbols::border, text::Line, widgets::{Block, Borders, Padding, Paragraph, Widget}
+    buffer::Buffer, layout::Rect, style::{Color, Style, Stylize}, symbols::border, text::Line, widgets::{Block, Padding, Paragraph, Widget}
 };
 
 use crate::state::{Change, ChangeKind};
@@ -21,35 +21,38 @@ impl<'a> Widget for &DiffPane<'a> {
     /*
      * This function implements list virtualisation for scrolling.
      * Files are not rendered until the number of lines consumed is equal to the scroll position.
-     * If part of the file is out of the viewport, scrolling_inside is true and only a slice of the
-     * diff is rendered.
+     * If only part of a file is out of the viewport, scrolling_inside is true and only the
+     * remainder of the file is rendered.
      * Rendered halts when the viewport height is full.
+     *
+     * Lines are defined to be the actual lines of content in the diffs.
+     * Rows are the visual lines in the UI that are rendered into.
      * */
     fn render(self, area: Rect, buf: &mut Buffer) {
         let outer = Block::new().padding(Padding::uniform(1));
-
         let inner = outer.inner(area);
 
         outer.render(area, buf);
 
-        let mut height_consumed: i16 = 0;
-        let mut height_filled: i16 = 0;
+        let mut rows_filled: i16 = 0;
+        let mut lines_consumed: i16 = 0;
+        let mut files_rendered: i16 = 0;
 
         for (file_name, diff_lines) in self.file_diffs {
-            if height_filled >= (inner.height as i16) {
+            if rows_filled >= (inner.height as i16) {
                 break;
             }
 
-            let cuts_off = (diff_lines.len() as i16) + height_filled > (inner.height as i16);
+            let diff_len = diff_lines.len() as i16;
 
-            let content_height: i16 = if cuts_off {
-                (inner.height as i16) - height_filled
-            } else {
-                diff_lines.len() as i16
-            };
+            let truncates = diff_len + rows_filled > (inner.height as i16);
 
-            height_consumed += content_height;
-            if height_consumed <= self.scroll_position {
+            let content_height: i16 = if truncates {
+                (inner.height as i16) - rows_filled
+            } else { diff_len };
+
+            if lines_consumed + content_height <= self.scroll_position {
+                lines_consumed += content_height;
                 continue;
             }
 
@@ -57,33 +60,20 @@ impl<'a> Widget for &DiffPane<'a> {
 
             let block = Block::bordered()
                 .title(title)
-                .border_set(border::PLAIN)
-                .borders(
-                    if cuts_off {
-                        Borders::TOP | Borders::LEFT | Borders::RIGHT
-                    } else {
-                        Borders::ALL
-                    }
-                );
+                .border_set(border::PLAIN);
 
-            let scrolling_inside = height_consumed - content_height < self.scroll_position && height_consumed >= self.scroll_position;
+            let scrolling_inside = files_rendered == 0 && self.scroll_position > 0;
+            let start_idx = if scrolling_inside {
+                std::cmp::max(self.scroll_position - lines_consumed, 0)
+            } else { 0 };
 
-            let start_idx: usize = if scrolling_inside {
-                content_height + self.scroll_position - height_consumed
-            } else { 0 } as usize;
-
-            let num_lines = (content_height as usize) - start_idx;
+            let num_rows = content_height - start_idx;
 
             let lines: Vec<Line> = diff_lines
                 .iter()
-                .skip(start_idx)
-                .take(num_lines)
-                .enumerate()
-                .map(|(index, change)| {
-                    if cuts_off && index == num_lines - 1 {
-                        return Line::from("  ...");
-                    }
-
+                .skip(start_idx as usize)
+                .take(num_rows as usize)
+                .map(|change| {
                     let prefix = match change.kind {
                         ChangeKind::Neutral => ' ',
                         ChangeKind::Insertion => '+',
@@ -103,11 +93,11 @@ impl<'a> Widget for &DiffPane<'a> {
                 .collect();
 
             // Add 2 for Block's top and bottom borders
-            let outer_height = num_lines + 2;
+            let outer_height = num_rows + 2;
 
             let space = Rect {
                 x: inner.x,
-                y: height_filled.try_into().expect("failed to cast height_filled as u16"),
+                y: rows_filled.try_into().expect("failed to cast height_filled as u16"),
                 height: outer_height.try_into().expect("failed to cast outer_height as u16"),
                 width: inner.width,
             };
@@ -116,7 +106,11 @@ impl<'a> Widget for &DiffPane<'a> {
                 .block(block)
                 .render(space, buf);
 
-            height_filled += outer_height as i16;
+            rows_filled += outer_height as i16;
+            files_rendered += 1;
+            if scrolling_inside {
+                lines_consumed += content_height - num_rows;
+            }
         }
     }
 }
